e0ffc38e754056be8b1cd819fd0192969489f69e
Alina Sheyko <asheyko@unc.edu>
2024-09-27 10:14:27 -0400

    cq04

---
+++ CQs/cq04/coordinates.py
@@ -0,0 +1,11 @@
+"""Coordinates"""
+
+__author__ = "730739029"
+
+
+def get_coords(xs: str, ys: str) -> None:
+    idx: int = 0
+    x_coord: str = xs[idx]
+    y_coord: str = ys[idx]
+    while idx<len(xs):
+       
---
+++ CQs/cq04/visualize.py
@@ -0,0 +1,12 @@
+from CQs.cq04.concatenation import concat
+
+"""Visualize"""
+
+__author__ = "730739029"
+
+x: str = "123"
+y: str = "abc"
+
+concat(x, y)
+
+print(concat(x, y))

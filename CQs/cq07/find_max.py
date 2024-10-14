eee7b28089f212453f7654e097d4cf84a933b8dc
Alina Sheyko <asheyko@unc.edu>
2024-10-14 17:10:58 -0400

    cq07 final

--- CQs/cq07/find_max.py
+++ CQs/cq07/find_max.py
@@ -3,16 +3,15 @@ __author__ = 730739029
 
 def find_and_remove_max(input1: list[int]) -> int:
     index: int = 0
-    max: int = input1[0]
-    if len(input1) == []:
+    max: int = 0  # had to make it 0 not input1[index] or erlse id get an error
+    if input1 == []:
         return -1
+    for x in input1:
+        if x > max:
+            max = x
     while index < len(input1):
-        if input1[index] > max:
-            max = input1[index]
-        index += 1
-    while index < len(input1):
-        index = input1[0]
         if input1[index] == max:
             input1.pop(index)
         index += 1
+    # print(input1)
     return max
--- CQs/cq07/max_test.py
+++ CQs/cq07/max_test.py
@@ -1,1 +1,19 @@
 __author__ = 730739029
+
+from CQs.cq07.find_max import find_and_remove_max
+
+
+def test_find_and_return_max() -> None:
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    assert find_and_remove_max(numbers) == 5
+
+
+def test_find_and_return_max1() -> None:
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    find_and_remove_max(numbers)
+    assert numbers == [1, 2, 3, 4]
+
+
+def test_find_and_return_max2() -> None:
+    numbers: list[int] = []
+    assert find_and_remove_max(numbers) == -1

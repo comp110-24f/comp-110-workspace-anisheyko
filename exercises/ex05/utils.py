0a768932a29d4bfe100496a8e9024412ecbbd062
Alina Sheyko <asheyko@unc.edu>
2024-10-16 19:43:38 -0400

    ex05 final

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -13,7 +13,7 @@ def only_evens(input1: list[int]) -> list[int]:
 
 
 def sub(a_list: list[int], start: int, end: int) -> list[int]:
-    """return theelements of a list that fall between the start and stop ints"""
+    """return the elements of a list that fall between the start and stop ints"""
     new_list: list[int] = []
     index: int = start
     stop: int = end - 1
@@ -21,7 +21,31 @@ def sub(a_list: list[int], start: int, end: int) -> list[int]:
         return []
     if index > stop:
         return []
+    if start < 0:
+        index = 0
+    if end > len(a_list) - 1:
+        stop = len(a_list) - 1
+        # you change the value of stop not end because stop is the local variable created inside the body
+    if end <= 0:
+        return []
+    # cant use elif statmeents because multiple might be true
     while index <= stop:
         new_list.append(a_list[index])
         index += 1
     return new_list
+
+
+def add_at_index(list_1: list[int], add_this: int, add_to: int) -> None:
+    """adds an int to a list at a specific index"""
+    if add_to < 0:
+        raise IndexError("Index is out of bounds for the input list")
+    if add_to > len(list_1):
+        raise IndexError("Index is out of bounds for the input list")
+    list_1.append(
+        0
+    )  # adds 0 to the end of the list so it can be replaced with add_this
+    x = len(list_1) - 1  # so it starts at the last index of the list
+    while x > add_to:
+        list_1[x] = list_1[x - 1]
+        x -= 1  # moves one index to the left, until it finds add_to
+    list_1[add_to] = add_this
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,3 +1,70 @@
 """define test functions for our utils"""
 
 __author__ = 730739029
+
+from exercises.ex05.utils import only_evens, sub, add_at_index
+
+
+def test_only_evens_edge_case() -> None:
+    """tests to make sure only evens works properly with an edge case"""
+    numbers: list[int] = []
+    assert only_evens(numbers) == []
+
+
+def test_only_evens_return() -> None:
+    """tests to make sure only evens returns the expected list"""
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    assert only_evens(numbers) == [2, 4]
+
+
+def test_only_evens_mutate() -> None:
+    """tests to make sure only evens doesnt mutate the original list"""
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    only_evens(numbers)  # call function without asserting first
+    assert numbers == [1, 2, 3, 4, 5]
+
+    # assert that input1 as numbers still returns the full list
+
+
+def test_sub_edge_case() -> None:
+    """tests to make sure sub returns an empty list when provided an empty list"""
+    numbers: list[int] = []
+    assert sub(numbers, 1, 3) == []
+
+
+def test_sub_return() -> None:
+    """tests to make sure that sub returns only even numers in the list"""
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    assert sub(numbers, 1, 3) == [2, 3]
+
+
+def test_sub_mutate() -> None:
+    """tests to make sure a_list is not mutated when sub is called"""
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    sub(numbers, 1, 3)
+    assert numbers == [1, 2, 3, 4, 5]
+
+
+import pytest
+
+
+def test_add_at_index_edge_case() -> None:
+    """tests to make sure IndexError is returned at edge cases"""
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    with pytest.raises(IndexError):
+        add_at_index(numbers, 3, -2)
+
+
+def test_add_at_index_return() -> None:
+    """tests to make sure None is returned, since the function has the return type None"""
+    numbers: list[int] = [6, 7, 9, 10]
+    assert (
+        add_at_index(numbers, 8, 2) == None
+    )  # none not the list b/c the return typer is none
+
+
+def test_add_at_index_mutate() -> None:
+    """tests to make sure the original list_1 IS mutated"""
+    numbers: list[int] = [6, 7, 9, 10]
+    add_at_index(numbers, 8, 2)
+    assert numbers == [6, 7, 8, 9, 10]

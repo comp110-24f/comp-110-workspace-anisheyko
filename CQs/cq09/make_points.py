e5a475faf3aad09f5ed5e3fa5768812f3f1e7b85
Alina Sheyko <asheyko@unc.edu>
2024-10-31 14:13:31 -0400

    working on cq09

--- CQs/cq09/make_points.py
+++ CQs/cq09/make_points.py
@@ -1,7 +1,13 @@
-"""checking Point methods fro correctness"""
+"""checking Point methods for correctness"""
 
 from CQs.cq09.point import Point
 
-point_1: Point = Point(1, 2)
+# declare original point
+point_1 = Point(1.0, 2.0)
+
+# test scale doesn't change point_1
 point_1.scale(2)
 print(point_1)
+
+# test scale_by
+point_1.scale_by(3)
